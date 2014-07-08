7e7f068bf01c65275774101e4eade33c2a917aab
Nayden Dochev <nayden.dochev@dnhsoft.com>
2014-07-08 08:15:01 +0300

    Added jquery and socket.io and simple communication between the server and browser

--- server/server.js
+++ server/server.js
@@ -1,31 +1,47 @@
 var express = require('express');
- 
-var server = express();
-server.use(express.static(__dirname + '/../public'));
- 
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/../public'));
+
 var port = 3000;
-server.listen(port, function() {
+http.listen(port, function() {
     console.log('server listening on port ' + port);
 });
 
-/*
-var http = require('http')
-  , sys  = require('sys');
+io.on('connection', function(socket) {
+    socket.on('msg', function(data) {
+        socket.emit('answer', {
+            res: "Message recieved"
+        });
+        console.log(data);
+    });
 
-var header = {
-	"Content-Type": "text/html"
-};
-
-// Handle incoming requests
-var app = http.createServer( function( request, response ) {
-
-	sys.puts("Incoming request! Woo!");
-	response.writeHeader(200, header);
-	response.write("<h1>Hello world!</h1>");
-	response.end();
-
+    setInterval(function() {
+        socket.broadcast.emit('ping', {
+            res: "Ping"
+        });
+    }, 3000);
 });
 
-// Listen for incoming requests on port 3000
-app.listen(3000);
-*/
+/*
+ var http = require('http')
+ , sys  = require('sys');
+ 
+ var header = {
+ "Content-Type": "text/html"
+ };
+ 
+ // Handle incoming requests
+ var app = http.createServer( function( request, response ) {
+ 
+ sys.puts("Incoming request! Woo!");
+ response.writeHeader(200, header);
+ response.write("<h1>Hello world!</h1>");
+ response.end();
+ 
+ });
+ 
+ // Listen for incoming requests on port 3000
+ app.listen(3000);
+ */
